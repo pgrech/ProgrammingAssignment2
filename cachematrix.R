e664dec560d9ab24a3771dcb06d7c208e1be6cac
pgrech <pdgrech@gmail.com>
2014-11-15 11:13:44 +0100

    Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions together yield the possibility to compute the inverse of a matrix in a way that the cache is accessed if the inverse is called for a second time. This way, the inverse only needs to be computed once which avoids time-consuming computations.  
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+## The following function creates the object "matrix".
+makeCacheMatrix  <- function(x = matrix()) {
+        I <- NULL ## initializes the variable for the inverse of x
+        set <- function(y) {
+                x <<- y
+                I <<- NULL
+        } ## definition of the "set" method which is used to set a new matrix
+        get <- function() x ## definition of the "get" method which returns the value of the matrix
+        setinverse <- function(inverse) I <<- inverse ## definition of the "setinverse" method which sets the inverse
+        getinverse <- function() I ## definition of the "getinverse" method with returns the value of I
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse) ##returns all methods
 }
 
 
-## Write a short comment describing this function
+## The following function returns a matrix that is the inverse of 'x'. If the inverse has already been computed before, cache is accessed.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+	
+        I <- x$getinverse() ## assigns the current value of the "inverse" to I - is either NULL or the actual inverse
+        if(!is.null(I)) {
+                message("getting cached data")
+                return(I)
+        } ## tests whether I already contains the inverse in which case nothing needs to be recomputed
+        data <- x$get()
+        I <- solve(data) ## computes the inverse
+        x$setinverse(I) ## assigns the correct value of the inverse in the matrix object 
+        I ## returns the inverse.
 }
+
